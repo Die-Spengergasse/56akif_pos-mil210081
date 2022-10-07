6f00d35fc509d1e09ce56d0526457ee70234f7ff
mil210081 <MIL210081@spengergasse.at>
2022-10-07 15:19:37 +0200

    Ubung_02

---
+++ Ubung_02/Properties/Properties/Lehrer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Properties
+{
+    public class Lehrer
+    {
+        public string Zuname { get; set; } = string.Empty;
+        public string Vorname { get; set; } = string.Empty;
+        public decimal? Bruttogehalt 
+        {
+            get { return _bruttogehalt; }
+            set
+            {
+                _bruttogehalt ??= value; 
+            }
+        }
+        private decimal? _bruttogehalt;
+
+        public string Kuerzel
+        {
+            get { return Zuname?.Substring(0,3)?.ToUpper() ?? " "; }
+        }
+
+        public decimal Nettogehalt
+        {
+            get { return _bruttogehalt ?? 0 * 0.8M; }
+        }
+    }
+}
---
+++ Ubung_02/Properties/Properties/Rechteck .cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Properties
+{
+    public class Rechteck
+    {   
+        public int Laenge 
+        { 
+          get { return _laenge; }
+          set { _laenge = value > 0 ? value : throw new ArgumentException("Ungültige Länge!"); } 
+        }
+        private int _laenge;
+
+        public int Breite
+        {
+            get { return _breite; }
+            // set { _breite = value > 0 ? value : throw new ArgumentException("Ungültige Länge!"); }
+            set { _breite = value < 0 ? throw new ArgumentException("Ungültige Länge!") : value; }
+
+        }
+        private int _breite;
+
+        // Property Flaeche ist read-only
+        public int Flaeche
+        {
+            get { return _laenge * _breite; }
+        }
+    }
+}
