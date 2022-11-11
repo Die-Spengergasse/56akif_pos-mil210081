2c6005dffc5e76132901714c124fa10fd709d0e3
mil210081 <MIL210081@spengergasse.at>
2022-11-11 16:02:50 +0100

    Test

---
+++ Test_11_Nov/Spg.PluePos.01/Spg.PluePos.01/Model/Post.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spg.PluePos._01.Model
+{
+    public abstract class Post
+    {
+        public string Title { get; }
+
+        public DateTime Created { get; }
+
+        public int Rating 
+        { 
+            get { return _rating; }
+            
+            set
+            {
+                if (value >= 1 && value <= 5)
+                {
+                    _rating = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Range muss zwischen 1 und 5 liegen!");
+                }
+            }
+        }
+        private int _rating;
+
+        public abstract string Html { get; }
+
+        public SmartPhoneApp? SmartPhone { get; set; } 
+
+        // Konstruktor
+        protected Post(string title, DateTime created)
+        {
+            if (title == null)
+            {
+                throw new ArgumentNullException("Titel war NULL");
+            }
+            Title = title;
+            Created = created;
+        }
+
+        public Post(string title) : this(title, DateTime.Now)
+        {
+        }
+
+        public override string ToString()
+        {
+            return Html;
+        }
+    }
+}
---
+++ Test_11_Nov/Spg.PluePos.01/Spg.PluePos.01/SmartPhoneApp.cs
@@ -0,0 +1,74 @@
+﻿using Spg.PluePos._01.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spg.PluePos._01
+{
+    public class SmartPhoneApp : List<Post>
+    {
+        public List<Post> posts { get; set; } = new List<Post>();
+
+        public string SmartPhoneId { get; set; } = string.Empty;
+
+        public SmartPhoneApp(string smartPhoneId)
+        {
+            SmartPhoneId = smartPhoneId;
+        }
+
+        public new void Add(Post p)
+        {
+            if (p != null)
+            {
+                this.posts.Add(p);
+                p.SmartPhone = this;
+            }
+        }
+
+        public string ProcessPosts()
+        {
+            string a = string.Empty;
+
+            foreach (Post post in this)
+            {
+                if (a != null)
+                {
+                    a.Concat(post.Html).Concat("\n");
+                }
+            }
+            return a;
+        }
+
+        public int CalcRating()
+        {
+            int a = 0;
+            foreach (Post post in this)
+            {
+                a += post.Rating;
+            }
+            return a;
+        }
+
+        /*
+        public string this[string title]
+        {
+            get
+            { return Post.[title].Html ?? null; }
+
+            Dictionary<string, Post> _posts = new Dictionary<string, Post>();
+            public Post SmartPhoneAppIterator(string title)
+            {
+                foreach (Post p in _posts)
+                {
+                    if (_posts[title].Html == "TextPost 6")
+                    {
+                        return p;
+                    }
+                }
+                return null;
+        }
+    }*/
+    }
+}
